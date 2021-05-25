08f2161f8921dde7354b8b8d9d3f5cc46a9fa48d
arrchon <80299255+arrchon@users.noreply.github.com>
2021-05-25 13:19:12 +0900

    20210525

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props){
-  return <h1>Halo, {props.name}</h1>;
+function formatDate(date){
+  return date.toLocaleDateString();
 }
 
-function App(){
+function Comment(props){
   return (
-    <div>
-      <Welcome name="web" />
-      <Welcome name="star" />
-      <Welcome name="light" />
-      <Welcome name="light2" />
+    <div className="comment">
+      <div className="user-info">
+        <img 
+          className="avatar" 
+          src={props.author.avatarUrl} 
+          alt={props.author.name} />
+        <div className="user-name"></div>
+      </div>
+      <div className="comment-text">{props.text}</div>
+      <div className="comment-date">{formatDate(props.date)}</div>
     </div>
-  );
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const comment = {
+  date: new Date(),
+  text: "열심히 할수록 기회는 따른다.",
+  author: {
+    name: "jhin",
+    avatarUrl: "http://yone10.dothome.co.kr/portfolio/assets/images/aboutImg02@2x.jpg"
+  },
+}
+
+ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author} />, document.getElementById("root"));
